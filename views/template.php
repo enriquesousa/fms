554f344687f2c61f5e20724e332559fe4855e12e
Enrique <enrique.sousa@gmail.com>
2025-01-31 11:52:07 -0800

    S01. 11. Maquetación del sitio

--- views/template.php
+++ views/template.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-	/*==============Capturar las rutas de la URL =======================================
+/*==============Capturar las rutas de la URL =======================================
 		Capturar las rutas de la URL 
 		No olvidar incluir el archivo .htaccess para servidores apache
 		Si usamos nginx no necesitamos este archivo.
@@ -10,66 +10,236 @@
 		Array ( [0] => logout [1] => exit )
 		=============================================*/
 
-		// $routesArray = explode("/",$_SERVER["REQUEST_URI"]);
-		// array_shift($routesArray); // Elimina la primera posición del array
-		// echo '<pre>'; print_r($routesArray); echo '</pre>';
+// $routesArray = explode("/",$_SERVER["REQUEST_URI"]);
+// array_shift($routesArray); // Elimina la primera posición del array
+// echo '<pre>'; print_r($routesArray); echo '</pre>';
 
-	$routesArray = explode("/",$_SERVER["REQUEST_URI"]);
-	array_shift($routesArray);
+$routesArray = explode('/', $_SERVER['REQUEST_URI']);
+array_shift($routesArray);
 
-	foreach ($routesArray as $key => $value) {	
-		$routesArray[$key] = explode("?",$value)[0]; // descarta los parámetros después del ?
-	}
-	// echo '<pre>'; print_r($routesArray); echo '</pre>';
+foreach ($routesArray as $key => $value) {
+    $routesArray[$key] = explode('?', $value)[0]; // descarta los parámetros después del ?
+}
+// echo '<pre>'; print_r($routesArray); echo '</pre>';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>FMS | File Manager System</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FMS | File Manager System</title>
 
-	<!-- Icono de la página -->
-	<link rel="icon" href="https://cdn.filestackcontent.com/1LIBQrPFRuGylthi5tUp">
+    <!-- Icono de la página -->
+    <link rel="icon" href="https://cdn.filestackcontent.com/1LIBQrPFRuGylthi5tUp">
 
-	<!-- Google Fonts -->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,600,500,700">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,600,500,700">
 
-	<!-- Bootstrap 5 -->
-	<link rel="stylesheet" href="/views/assets/plugins/bootstrap5/bootstrap.min.css">
-	<!-- https://icons.getbootstrap.com/; lo hacemos con CDN por las fonts que son de Bootstrap y no las descargamos-->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
+    <!-- Bootstrap 5 Ver módulos: https://www.w3schools.com/bootstrap5/bootstrap_get_started.php -->
+    <link rel="stylesheet" href="/views/assets/plugins/bootstrap5/bootstrap.min.css">
 
-	<!-- https://fontawesome.com/v5/search -->
-	<link rel="stylesheet" href="/views/assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- Bootstrap 5 Icons https://icons.getbootstrap.com/; lo hacemos con CDN por las fonts que son de Bootstrap y no las descargamos-->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
 
-	<!-- jQuery UI https://jqueryui.com -->
-	<link rel="stylesheet" href="/views/assets/plugins/jquery-ui/jquery-ui.css">
+    <!-- https://fontawesome.com/v5/search -->
+    <link rel="stylesheet" href="/views/assets/plugins/fontawesome-free/css/all.min.css">
 
-	<!-- Material Preloader -->
-	<link rel="stylesheet" href="/views/assets/plugins/material-preloader/material-preloader.css">
+    <!-- jQuery UI https://jqueryui.com -->
+    <link rel="stylesheet" href="/views/assets/plugins/jquery-ui/jquery-ui.css">
 
-	<!-- Toastr Demo: https://codeseven.github.io/toastr/demo.html GitHub: https://github.com/CodeSeven/toastr Site: https://codeseven.github.io/toastr/-->
-	<link rel="stylesheet" href="/views/assets/plugins/toastr/toastr.min.css">
+    <!-- Material Preloader -->
+    <link rel="stylesheet" href="/views/assets/plugins/material-preloader/material-preloader.css">
 
-	<!-- FMS CSS Nuestro Propio CSS -->
-	<link rel="stylesheet" href="/views/assets/css/fms/fms.css">
+    <!-- Toastr Demo: https://codeseven.github.io/toastr/demo.html GitHub: https://github.com/CodeSeven/toastr Site: https://codeseven.github.io/toastr/-->
+    <link rel="stylesheet" href="/views/assets/plugins/toastr/toastr.min.css">
 
-	<!-- *********************************** -->
-	<!-- JavaScripts 					    -->
-	<!-- *********************************** -->
-	<script src="/views/assets/plugins/jquery/jquery.min.js" defer></script>
-	<script src="/views/assets/plugins/jquery-ui/jquery-ui.js" defer></script>
-	<script src="/views/assets/plugins/bootstrap5/bootstrap.bundle.min.js" defer></script>
-	<script src="/views/assets/plugins/sweetalert/sweetalert.min.js" defer></script>
-	<script src="/views/assets/plugins/material-preloader/material-preloader.js" defer></script>
-	<script src="/views/assets/plugins/toastr/toastr.min.js" defer></script>
+    <!-- FMS CSS Nuestro Propio Custom CSS -->
+    <link rel="stylesheet" href="/views/assets/css/fms/fms.css">
+
+    <!-- *********************************** -->
+    <!-- JavaScripts 					    -->
+    <!-- *********************************** -->
+    <script src="/views/assets/plugins/jquery/jquery.min.js" defer></script>
+    <script src="/views/assets/plugins/jquery-ui/jquery-ui.js" defer></script>
+    <script src="/views/assets/plugins/bootstrap5/bootstrap.bundle.min.js" defer></script>
+    <script src="/views/assets/plugins/sweetalert/sweetalert.min.js" defer></script>
+    <script src="/views/assets/plugins/material-preloader/material-preloader.js" defer></script>
+    <script src="/views/assets/plugins/toastr/toastr.min.js" defer></script>
 
 </head>
 
 <body>
-	<h1>Hola Mundo, soy la plantilla!</h1>
+    
+	<!--=== TOP NAV BAR ============================================
+		TOP NAV BAR
+		======================================================-->
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark" id="top">
+	  <div class="container">
+	    <ul class="navbar-nav">
+	      <li class="nav-item">
+	        <a class="nav-link active" href="/">File Manager System</a>
+	      </li>
+	    </ul>
+	    <div class="d-flex">
+	    	<a href="#myLogin" class="ms-auto px-3 text-white" data-bs-toggle="modal">Iniciar sesión</a>
+	    </div>
+	  </div>
+	</nav>
+
+	<!--=== CONTENT ============================================
+		CONTENT
+		======================================================-->
+	<div class="container-fluid p-4 min-vh-100" id="content">
+		<div class="container bg-white border rounded">
+
+			<!--=== SEARCH & BUTTONS =================
+				SEARCH & BUTTONS
+				======================================-->
+			<div class="row py-4 px-4 pb-2">
+
+				<!--=== 'BUSCADOR' 
+					 'BUSCADOR' 
+					 ======================================-->
+				<div class="col-12 col-lg-6">
+			  		<div class="input-group mt-1">
+			  			<input type="text" class="form-control rounded-start" placeholder="Busca archivos">
+			  			<button type="button" class="input-group-text rounded-end"><i class="bi bi-search"></i></button>
+			  		</div>
+				</div>
+				
+				<!--=== 'BOTONES' 
+					 'BOTONES' 
+					 ======================================-->
+				<div class="col-12 col-lg-6">
+				  	<div class="d-flex flex-row-reverse">
+					  	<div class="p-1">
+						  <button type="button" class="btn btn-sm py-2 px-3 bg-info-50 font-weight-bold rounded"><i class="bi bi-arrow-up-circle pe-1"></i> Iniciar Subida</button>
+					  	</div>
+					  	<div class="p-1">
+						  <button type="button" class="btn btn-sm py-2 px-3 bg-success-50 font-weight-bold rounded"><i class="bi bi-plus-lg pe-1"></i> Agregar Archivos</button>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<!--=== Carpetas y Filtros ============================
+				 'Carpetas y Filtros' 
+				 ======================================-->
+			<div class="row pb-4 px-4 py-1">
+				
+				<!--=== Carpetas
+					 Carpetas o Servidores donde se subirán los archivos
+					 Pueden ser: Servidores, AWS S3, Google Drive, Dropbox
+					 ======================================-->
+				<div class="col-12 col-lg-7 mt-3">
+			  		<div class="row">
+
+						<!-- Servidor Local -->
+			  			<div class="col">
+			  				<div class="form-check">
+			  					<input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+			  					<label class="form-check-label ps-1 align-middle">Servidor</label>
+			  				</div>
+			  			 	<div class="progress mt-1" style="height:10px">
+			  					<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width:90%">90%</div>
+			  				</div>
+			  		  	</div>
+
+						<!-- AWS S3 -->
+			  		  	<div class="col">
+			  				<div class="form-check">
+			  					<input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+			  					<label class="form-check-label ps-1 align-middle">AWS S3</label>
+			  				</div>
+			  				<div class="progress mt-1" style="height:10px">
+			  					<div class="progress-bar progress-bar-striped progress-bar-animated bg-success" style="width:20%">20%</div>
+			  				</div>
+			  		  	</div>
+
+						<!-- Cloudinary -->
+			  		  	<div class="col">
+			  				<div class="form-check">
+			  					<input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+			  					<label class="form-check-label ps-1 align-middle">Cloudinary</label>
+			  				</div>
+			  				<div class="progress mt-1" style="height:10px">
+			  					<div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" style="width:60%">60%</div>
+			  				</div>
+			  		  	</div>
+
+						<!-- Vimeo -->
+			  		  	<div class="col">
+			  				<div class="form-check">
+			  					<input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+			  					<label class="form-check-label ps-1 align-middle">Vimeo</label>
+			  				</div>
+			  				<div class="progress mt-1" style="height:10px">
+			  					<div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width:80%">80%</div>
+			  				</div>
+			  		  	</div>
+
+						<!-- Mailchimp -->
+			  		  	<div class="col">
+			  				<div class="form-check">
+			  					<input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+			  					<label class="form-check-label ps-1 align-middle">Mailchimp</label>
+			  				</div>
+			  				<div class="progress mt-1" style="height:10px">
+			  					<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width:90%">90%</div>
+			  				</div>
+			  		  	</div>
+
+			  		</div>
+			  		
+				</div>
+
+				<!--=== 'Filtros' 
+					 'Filtros' 
+					 ======================================-->
+				<div class="col-12 col-lg-5 mt-3">
+			  		<div class="d-flex flex-row-reverse">
+
+			  			<div class="btn-group rounded">
+			  				<button type="button" class="btn btn-default border rounded-start text-secondary changeView" module="grid">
+			  					<i class="bi bi-grid-3x3-gap-fill"></i>
+			  				</button>
+			  				<button type="button" class="btn btn-default border rounded-end bg-dark changeView text-white" module="list">
+			  					<i class="bi bi-list"></i>
+			  				</button>
+			  			</div>
+
+			  			<select class="form-select rounded mx-2" id="sortBy">
+						  <option value="">Ordenar por</option>
+						  <option value="date_updated_file-DESC">Nuevo primero</option>
+						  <option value="date_updated_file-ASC">Antiguo primero</option>
+						  <option value="size_file-DESC">Mas grande primero</option>
+						  <option value="size_file-ASC">Mas chico primero</option>
+						  <option value="name_file-ASC">A-Z</option>
+						  <option value="name_file-DESC">Z-A</option>
+						</select>
+
+						<select class="form-select rounded" id="filterBy">
+						  <option value="">Filtrar por</option>
+						  <option value="ALL">Todos</option>
+						  <option value="images/JPG">imágenes/JPG</option>
+						  <option value="video/mp4">video/mp4</option>
+						  <option value="application/pdf">aplicación/pdf</option>
+						  <option value="application/zip">aplicación/zip</option>
+						</select>
+			  			
+			  		</div>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+
 </body>
+
 </html>
